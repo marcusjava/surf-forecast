54d6f6b12e34b28c79b556f7589544f3df78c963
Marcus Vinicius <buba.sergipe@gmail.com>
2021-12-26 16:57:04 -0300

    Pino for logging created

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,9 +1,9 @@
 import { ClassMiddleware, Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { Beach } from '@src/models/beach';
-import mongoose from 'mongoose';
 import { BaseController } from './index';
 import { authMiddleware } from '@src/middlewares/auth';
+import logger from '@src/logger';
 
 @Controller('beaches')
 @ClassMiddleware(authMiddleware)
@@ -15,6 +15,7 @@ export class BeachesController extends BaseController {
       const result: Beach = await beach.save();
       res.status(201).send(result);
     } catch (error: any) {
+      logger.error(error);
       this.sendCreatedUpdatedErrorResponse(res, error);
     }
   }
--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -3,6 +3,7 @@ import { Forecast } from '@src/services/forecast';
 import { Request, Response } from 'express';
 import { Beach } from '@src/models/beach';
 import { authMiddleware } from '@src/middlewares/auth';
+import logger from '@src/logger';
 
 const forecast = new Forecast();
 
@@ -18,7 +19,8 @@ export class ForecastController {
       const beaches = await Beach.find({ user: req.decoded?.id });
       const forecastData = await forecast.processForecastForBeaches(beaches);
       res.status(200).send(forecastData);
-    } catch (error) {
+    } catch (error: any) {
+      logger.error(error);
       res.status(500).send({ error: 'Something went wrong' });
     }
   }
